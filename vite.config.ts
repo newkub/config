f4cb478cf94fc718f043062d0f82ffd4f1a2951a
newkub <newkubise@gmail.com>
2025-05-18 17:26:03 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,23 @@
 import UnoCSS from "@unocss/vite";
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import vueDevTools from "vite-plugin-vue-devtools";
+import { groupIconVitePlugin } from "vitepress-plugin-group-icons";
+import AutoImport from "unplugin-auto-import/vite";
+import UnpluginUnused from 'unplugin-unused/vite';
+
+export default defineConfig({
+    base: 'https://utc.yuy1n.io/',
+    plugins: [
+        vueDevTools(),
+        AutoImport(),
+        tsconfigPaths(),
+checker({
+            typescript: true,
+            vueTsc: true,
+            biome: {
+                command: "lint",
+            },
+            overlay: false,
+        }),
