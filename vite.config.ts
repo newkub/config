2c99303e95dfcbe0e933347595fc473267443c46
newkub <newkubise@gmail.com>
2025-05-18 17:30:52 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,13 @@
 import UnoCSS from "unocss/vite";
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import vueDevTools from "vite-plugin-vue-devtools";
+import { groupIconVitePlugin } from "vitepress-plugin-group-icons";
+import AutoImport from "unplugin-auto-import/vite";
+import UnpluginUnused from 'unplugin-unused/vite';
+import checker from 'vite-plugin-checker';
+import TurboConsole from 'unplugin-turbo-console/vite'
+
+export default defineConfig({
+    plugins: [
