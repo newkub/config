9a81d22b2af2634a70cb3a84d63b960fa08fff6b
newkub <newkubise@gmail.com>
2025-05-18 17:27:04 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,13 @@
 // vite.config.ts
 import { defineConfig } from "vite";
+import UnoCSS from "unocss/vite";
+import { groupIconVitePlugin } from "vitepress-plugin-group-icons";
+import UnpluginUnused from 'unplugin-unused/vite'
+import TurboConsole from 'unplugin-turbo-console/vite'
+
+export default defineConfig({
+	plugins: [
+		UnoCSS(),
+		UnpluginUnused(),
+		TurboConsole(),
+		groupIconVitePlugin({
