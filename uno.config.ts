39b377e1deb838b1180ec068e0c5e6d5caa7223a
newkub <newkubise@gmail.com>
2025-05-30 01:44:04 +0700

    Update uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, presetIcons, presetWind4, transformerVariantGroup, presetWebFonts, transformerCompileClass } from "unocss";
+import { defineConfig, presetIcons, presetWind4, transformerVariantGroup, presetWebFonts, transformerCompileClassม } from "unocss";
 import { createLocalFontProcessor } from '@unocss/preset-web-fonts/local';
 import { presetRemToPx } from '@unocss/preset-rem-to-px';
 
