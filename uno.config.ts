1774a06324d5b7a9219ebc7fde195be308a81288
newkub <newkubise@gmail.com>
2025-05-30 01:36:28 +0700

    Update uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -1,1 +1,25 @@
 import { defineConfig, presetIcons, presetWind4, transformerVariantGroup, presetWebFonts } from "unocss";
+import { createLocalFontProcessor } from '@unocss/preset-web-fonts/local';
+
+export default defineConfig({
+    presets: [
+        presetWind4(),
+        presetIcons({
+            extraProperties: {
+                display: "inline-block",
+                "vertical-align": "middle",
+            },
+            collections: {
+                mdi: () =>
+                    import("@iconify-json/mdi/icons.json").then((i) => i.default),
+            },
+        }),
+        presetWebFonts({
+            provider: 'none',
+            fonts: {
+                sans: 'Roboto',
+                mono: 'Fira Code',
+            },
+            processors: createLocalFontProcessor({
+                cacheDir: 'node_modules/.cache/unocss/fonts',
+                fontAssetsDir: 'public/assets/fonts',
