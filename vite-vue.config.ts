430785612c132fe48eb4c122cebc63e2e1a62a1b
newkub <newkubise@gmail.com>
2025-05-30 02:57:16 +0700

    Create vite-vue.config.ts

---
+++ vite-vue.config.ts
@@ -0,0 +1,26 @@
+import UnoCSS from "unocss/vite";
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import vueDevTools from "vite-plugin-vue-devtools";
+import checker from 'vite-plugin-checker';
+import TurboConsole from 'unplugin-turbo-console/vite'
+
+export default defineConfig({
+    plugins: [
+        vueDevTools(),
+        tsconfigPaths(),
+        checker({
+            typescript: true,
+            vueTsc: true,
+            biome: {
+                command: "lint",
+            },
+            overlay: false,
+        }),
+        UnoCSS(),
+        ViteImageOptimizer({}),
+        UnpluginUnused(),
+        TurboConsole(),
+    ],
+});
