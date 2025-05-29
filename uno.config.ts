2dbd8cff438fdd1159b9ce36ba9c48cafe6bf885
newkub <newkubise@gmail.com>
2025-05-30 01:56:16 +0700

    Update uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -33,7 +33,9 @@ export default defineConfig({
         colors: {
             Background: 'hsl(var(--background-light))',
             Text: 'hsl(var(--text-light))',
-            brand: '
+            brand: {
+                
+            }
             Alert: 'hsl(var(--alert-light))',
             Brand: 'hsl(var(--brand-light))',
             Error: 'hsl(var(--error-light))',
