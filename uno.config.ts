6c1c7d602a6d86e95d0db26455814cee61d0e438
newkub <newkubise@gmail.com>
2025-05-30 01:45:02 +0700

    Update uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -4,7 +4,6 @@ import { createLocalFontProcessor } from '@unocss/preset-web-fonts/local';
 export default defineConfig({
     presets: [
         presetWind4(),
-        presetRemToPx(),
         presetIcons({
             extraProperties: {
                 display: "inline-block",
