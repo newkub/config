148028666adad944a16567d3d76c80d865af3e60
newkub <newkubise@gmail.com>
2025-05-30 01:35:10 +0700

    Update uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
             background: 'var(--background-light)',
             text: 'var(--text-light)',
             alert: 'var(--alert-light)',
-            colorB: 'var(--brand-light)',
+            colorBrand: 'var(--brand-light)',
             error: 'var(--error-light)',
             success: 'var(--success-light)',
         },
