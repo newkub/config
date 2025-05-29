8b533297ecb1563fe60a9a597cb57d9f20e67409
newkub <newkubise@gmail.com>
2025-05-30 02:59:05 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
         UnoCSS(),
         ViteImageOptimizer({}),
         TurboConsole(
-            highlight: t,
+            highlight: true,
         ),
     ],
 });
