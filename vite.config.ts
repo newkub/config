904330db700c08e424b20ca848ca2f3e1bc17513
newkub <newkubise@gmail.com>
2025-05-30 02:55:03 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import vueDevTools from "vite-plugin-vue-devtools";
-import { groupIconVitePlugin } from "vitepress-plugin-group-icons";
 import AutoImport from "unplugin-auto-import/vite";
 import UnpluginUnused from 'unplugin-unused/vite';
 import checker from 'vite-plugin-checker';
