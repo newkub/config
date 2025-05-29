64779923a70331a8b969b0cab009290fbc7ee440
newkub <newkubise@gmail.com>
2025-05-30 01:36:30 +0700

    Update uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -23,3 +23,21 @@ export default defineConfig({
             processors: createLocalFontProcessor({
                 cacheDir: 'node_modules/.cache/unocss/fonts',
                 fontAssetsDir: 'public/assets/fonts',
+                fontServeBaseUrl: '/assets/fonts'
+            })
+        }),
+    ],
+    transformers: [transformerVariantGroup()],
+    theme: {
+        colors: {
+            Background: 'var(--background-light)',
+            Text: 'var(--text-light)',
+            Alert: 'var(--alert-light)',
+            Brand: 'var(--brand-light)',
+            Error: 'var(--error-light)',
+            Success: 'var(--success-light)',
+        },
+    },
+    shortcuts: [
+        ['btn', 'px-4 py-2 rounded-lg font-medium transition-colors duration-300'],
+        ['btn-primary', 'bg-Brand text-white hover:bg-Brand/90'],
