531779595dd4aab009960d358b46878f74d9c372
newkub <newkubise@gmail.com>
2025-06-21 05:37:42 +0700

    Update uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -3,7 +3,7 @@ import { createLocalFontProcessor } from '@unocss/preset-web-fonts/local';
 
 export default defineConfig({
     presets: [
-        presetMini(),
+        presetMW(),
         presetIcons({
             collections: {
                 mdi: () =>
